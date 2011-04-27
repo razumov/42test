19532d768b48dc09e6bec38c12f0791e5dbac493
Denis Razumov <Shkiperr@yahoo.com>
2011-04-27 17:01:24 +0300

    first commit

---
+++ app/tests.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from models import Person
+
+
+class SimpleTest(TestCase):
+
+    def test_contacts(self):
+        # A response
+        response = self.client.get('/')
+        # Check response status
+        self.failUnlessEqual(response.status_code, 200)
+        # Check page information
+        p = Person.objects.get(pk=1)
+        self.assertContains(response, p.name, 1)
+        self.assertContains(response, p.surname, 1)
+        self.assertContains(response, p.bio, 1)
+        self.assertContains(response, p.contacts, 1)
