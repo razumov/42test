bf3d38303b2ee57c26a6a75a9660a349819a8bca
Denis Razumov <Shkiperr@yahoo.com>
2011-04-29 17:55:29 +0300

    third commit

--- app/views.py
+++ app/views.py
@@ -4,9 +4,11 @@ from models import Person
 
 
 
+
 def person_views (request):
     p = Person.objects.all()[0]
     return render_to_response ('person.html', {'p' : p})
     
 
+	
 
--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 # Django settings for test42 project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -12,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/denis/workspace/test42/src/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.join(os.path.abspath(os.path.dirname(__file__)),'sqlite.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
